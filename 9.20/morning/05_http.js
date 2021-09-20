ff6ec4bbb011f088fcfa844e27086b3aa4613583
noon000 <764020793@qq.com>
2021-09-20 10:39:52 +0800

    feat:catalogue changed

---
+++ 9.20/morning/05_http.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((request, response) => {
+    //console.log('a request',getPrototypeChain(request));
+    //response.end('hi node');
+    const { url, method } = request;
+    if (url === '/' && method === "GET") {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                response.writeHead(500, {
+                    'Content-Type': 'text/plain;charset=utf-8'
+                })
+                response.end('500 看到这个说明你的服务器挂了');
+                return
+            }
+            response.statusCode = 200;
+            response.setHeader('Content-type', 'text/html');
+            response.end(data)
+        })
+    }
+    else {
+        response.statusCode == 404
+        response.setHeader("Content-Type", 'text/plain;charset = utf-8');
+        response.end('404 没找到')
+    }
+})
+    .listen(3002, () => {
+        console.log('sever at 3002')
+    })
+function getPrototypeChain(obj) {
+    const protoChain = [];
+    while (obj = Object.getPrototypeOf(obj)) {
+        protoChain.push(obj)
+    }
+    return protoChain;
+}
