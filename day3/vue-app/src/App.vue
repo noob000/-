380a5dbec4fedacb4e149b9b79f200b1230f226f
noon000 <764020793@qq.com>
2021-09-22 18:11:28 +0800

    feat:achieve dragable

--- day3/vue-app/src/App.vue
+++ day3/vue-app/src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <div id="sidebar">
+      <span @mousedown="dragRow">row1</span>
+      <span @mousedown="dragRow">col</span>
+      <span @mousedown="dragRow">button</span>
+    </div>
+    <div id="editpanel">
+      <div class="row">
+        <div class="col">
+          <button></button>
+        </div>
+        <div class="col"></div>
+      </div>
+    </div>
+    <div id="dragable" :style="{left:y,top:x}" v-if="drag.isDragging ">dragable</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    drag: {
+      x0: 0,
+      y0: 0,
+      dx: 0,
+      dy: 0,
+      x: 0,
+      y: 0,
+      isDragging: false,
+    },
+  }),
+  components: {},
+  methods: {
+    dragRow() {
+      console.log("start");
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  position: absolute;
+}
+#sidebar {
+  width: 160px;
+  background-color: whitesmoke;
+}
+#sidebar > * {
+  width: 60px;
+  height: 60px;
+  margin: 10px;
+  background-color: green;
+  display: inline-block;
+  box-sizing: border-box;
+  border: 1px solid black;
+  user-select: none;
+}
+#editpanel {
+  flex: 1;
+}
+.row {
+  width: 100%;
+  min-height: 50px;
+  background-color: yellow;
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+}
+.col {
+  height: 100px;
+  background-color: lightgreen;
+  width: 100px;
+  margin: 10px;
+}
+#dragable {
+  position: absolute;
+  width: 60px;
+  height: 60px;
 }
 </style>
